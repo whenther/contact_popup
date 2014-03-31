d776efdf22cf6c78baa8e51bbbf75c78fd6ae6c5
Will <will@Watson.(none)>
2014-03-31 02:16:37 -0700

    Finish design

--- contact.js
+++ contact.js
@@ -1,40 +1,79 @@
 $(function () {
+    //------------//
+    // vars       //
+    //------------//
     // get some references
-    var contact$ = $('.contact');
-    var pop$ = $('.pop-container');
-    var both$ = $('.contact, .pop-container');
+    var contact$ = $('.contact'),
+        pop$ = $('.pop-container'),
+        both$ = $('.contact, .pop-container');
+    
+    // set up pop state
+    var popup = {
+        locked: false,
+        up: false
+    };
     
     // timer
     var timeout = null;
     
+    //------------//
+    // setup pop  //
+    //------------//
+    var popup$ = 
+    
+    //------------//
+    // callbacks  //
+    //------------//
     // open popup
     var popUp = function () {
-        clearTimeout(timeout);
-        pop$.fadeIn('fast');
+        if (!(popup.locked)) {
+            clearTimeout(timeout);
+            pop$.fadeIn('fast');
+            popup.up = true;
+        }
     };
     // close popup
     var popDown = function () {
-        timeout = setTimeout(function () {
-            pop$.fadeOut('fast');
-        }, 250);
+        if (!(popup.locked)) {
+            timeout = setTimeout(function () {
+                pop$.fadeOut('fast');
+                popup.up = false;
+            }, 250);
+        }
     };
     
     // make an image bigger
     var imgBig = function () {
-        $(this).animate({
+        $(this).stop().animate({
             'width': '40px'
         }, 'fast');
     };
     // make an image smaller
     var imgSml = function () {
-        $(this).animate({
+        $(this).stop().animate({
             'width': '32px'
         }, 'fast');
     };
     
-    //$(this).width * 0.8 + 'px'
+    //------------//
+    // listeners  //
+    //------------//
     // hover listener
     both$.hover(popUp, popDown);
+    both$.children('.icon img').click(function (e) {
+        e.stopPropagation();
+    });
+    // click listener
+    both$.click(function (e) {
+        if (popup.locked) {
+            popup.locked = false;
+            popDown();
+        }
+        else {
+            popUp();
+            popup.locked = true;
+        }
+    });
     // icon listener
     $('.icon img').hover(imgBig, imgSml);
 });
