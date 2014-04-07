df1101528a0dea0d2ad4d1503997d5c5bb897797
whenther <william.lee.wagner@gmail.com>
2014-04-07 02:16:08 -0700

    Start transition from HTML to jQuery

--- contact.js
+++ contact.js
@@ -1,14 +1,36 @@
 $(function () {
+    // build a pop-up
+    var buildPop = function () {
+        var pop$ = $('<div class="pop-container">').append(
+            $('<div class="pop">').append(
+                $('<form>').
+                append('<p class="email-label">').
+                append('<input type="test" name="email-from" id="email-from" placeholder="Your email address"/>').
+                append('<span class="warn">Please enter a valid email address.</span>').
+                append('<textarea id="email">').
+                append('<span class="warn">Please enter a message.</span>').
+                append('<input class="submit" type="button" value="Submit"/>')
+            ).append(
+                $('<div class="icon-container">').
+                append('<p>Follow me on social media!</p>').
+                append('<div class="icons">')
+            )
+        )
+        
+        return pop$;
+    };    
+    
     //------------//
     // vars       //
     //------------//
+  
     // get some references
     var contact$ = $('.contact'),
         pop$ = $('.pop-container'),
         both$ = $('.contact, .pop-container');
     
     // set up pop state
-    var popup = {
+    var popup = { 
         locked: false,
         up: false
     };
